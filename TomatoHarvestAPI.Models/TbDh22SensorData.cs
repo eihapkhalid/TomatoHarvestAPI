7c99ce6c9acdfe925d6e871d34665849d34dbb5e
eihapKhalid <eihapinfo@gmail.com>
2023-08-14 15:10:02 +0300

    Create table of models : (TbDh22SensorData - TbLdrSensorData - TbSoilMoistureSensorData)

---
+++ TomatoHarvestAPI.Models/TbDh22SensorData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomatoHarvestAPI.Models
+{
+    public class TbDh22SensorData
+    {
+        public int Dh22SensorDataId { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+    }
+}
---
+++ TomatoHarvestAPI.Models/TbLdrSensorData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomatoHarvestAPI.Models
+{
+    public class TbLdrSensorData
+    {
+        public int LdrSensorDataId { get; set; }
+        public int LightIntensity { get; set; }
+    }
+}
