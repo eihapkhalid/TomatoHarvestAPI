010c74d743f98acd03ac0c9f2e7e385540cf9259
eihapKhalid <eihapinfo@gmail.com>
2023-08-14 16:00:13 +0300

    create IUnitOfWork pattern

---
+++ TomatoHarvestAPI.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomatoHarvestAPI.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        void Save();
+        public IDh22SensorDataRepository TbTbDh22SensorData { get; }
+        public ILdrSensorDataRepository TbLdrSensorData { get; }
+        public ISoilMoistureSensorDataRepository TbSoilMoistureSensorData { get; }
+
+    }
+}
