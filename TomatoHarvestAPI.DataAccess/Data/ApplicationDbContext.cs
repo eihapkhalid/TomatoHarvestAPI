0c1f92edb61d2d534cccb4b971d0dad93883583a
eihapKhalid <eihapinfo@gmail.com>
2023-08-14 15:18:24 +0300

    Create ApplicationDbContext file in Data folder

---
+++ TomatoHarvestAPI.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomatoHarvestAPI.Models;
+
+namespace TomatoHarvestAPI.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<TbDh22SensorData> TbDh22SensorDatas { get; set; }
+        public DbSet<TbLdrSensorData> TbLdrSensorDatas { get; set; }
+        public DbSet<TbSoilMoistureSensorData> TbSoilMoistureSensorDatas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        }
+}
