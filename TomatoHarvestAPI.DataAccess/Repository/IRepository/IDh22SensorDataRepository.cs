a20edad141de5d2fcb814031051fa3ed7ac82f0b
eihapKhalid <eihapinfo@gmail.com>
2023-08-14 15:55:16 +0300

    Create IRepository, IDh22SensorDataRepository, ILdrSensorDataRepository and ISoilMoistureSensorDataRepository

---
+++ TomatoHarvestAPI.DataAccess/Repository/IRepository/IDh22SensorDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomatoHarvestAPI.Models;
+
+namespace TomatoHarvestAPI.DataAccess.Repository.IRepository
+{
+    public interface IDh22SensorDataRepository : IRepository<TbDh22SensorData>
+    {
+        void Update(TbDh22SensorData obj);
+    }
+}
---
+++ TomatoHarvestAPI.DataAccess/Repository/IRepository/ILdrSensorDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomatoHarvestAPI.Models;
+
+namespace TomatoHarvestAPI.DataAccess.Repository.IRepository
+{
+    public interface ILdrSensorDataRepository : IRepository<TbLdrSensorData>
+    {
+        void Update(TbLdrSensorData obj);
+    }
+}
