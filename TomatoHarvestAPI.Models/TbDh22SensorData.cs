ed4486584b6cc2d48d91d30401d2e18c37d2fa21
eihapKhalid <eihapinfo@gmail.com>
2023-08-15 10:56:00 +0300

    update

--- TomatoHarvestAPI.Models/TbDh22SensorData.cs
+++ TomatoHarvestAPI.Models/TbDh22SensorData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,9 @@ namespace TomatoHarvestAPI.Models
 {
     public class TbDh22SensorData
     {
+        [Key]
         public int Dh22SensorDataId { get; set; }
+        public string Dh22SensorIdentifier { get; set; } //رقم مميز لكل حساس
         public double Temperature { get; set; }
         public double Humidity { get; set; }
     }
--- TomatoHarvestAPI.Models/TbLdrSensorData.cs
+++ TomatoHarvestAPI.Models/TbLdrSensorData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace TomatoHarvestAPI.Models
 {
     public class TbLdrSensorData
     {
+        [Key]
         public int LdrSensorDataId { get; set; }
+        public string LdrSensorIdentifier { get; set; } //رقم مميز لكل حساس
         public int LightIntensity { get; set; }
     }
 }
